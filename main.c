58a91f4233d33d4ca1cef291dbed08597279ba48
MegaPixel <dhunganakunjan1@gmail.com>
2021-03-07 21:47:59 +0545

    set keypress duration

--- main.c
+++ main.c
@@ -3,21 +3,26 @@
 #include<windows.h>
 #include "frequency.h"
 
-int makeSound(int keycode) {
+int makeSound(int keycode, int dur) {
     float freq = getFrequency(keycode);
     if (freq < 1) return 0;
     
-    return Beep(freq, 500);
+    return Beep(freq, dur);
 }
  
 void main() {
-    int keycode;
- 
+    int keycode, dur = 500;
+    
+    printf("\nEnter keypress duration in ms: ");
+    scanf("%d", &dur);
+
+    if (dur < 1) dur = 500;
+
     printf("\nApp started! Press ctrl+c to exit!\n");
- 
+
     while(1) {
         if (keycode == 3) break;
         keycode = getch();
-        makeSound(keycode);
+        makeSound(keycode, dur);
     }
 }
